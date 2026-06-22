adc2146b5f9abd7aad9be8c566cb36490dc97bf4
Jonathan <jonathan.a.santos10@aluno.senai.br>
2026-06-22 20:54:31 -0300

    adicionado comentários para explicar o código.

--- js/script_veiculo.js
+++ js/script_veiculo.js
@@ -21,22 +21,22 @@ const veiculo = { // gerando os elementos que serão inseridos no array
     
 }
 
-addVeiculo(veiculo)
+addVeiculo(veiculo) //aqui eu chamo o push para cadastrar um veiculo no array
 
-// limpando o form
+// isso aqui só limpa o formulário quando o usuário da submit.
 formVeiculo.reset()
 })
 
 // dando push dos dados no array
 const addVeiculo = (objVeiculo) => {
-    veiculos.push(objVeiculo)
+    veiculos.push(objVeiculo) 
 
-    listaVeiculos()
+    listaVeiculos() // chamando a função de listar, se isso não estiver aqui o código não sabe o que listar.
 }
 
-const listaVeiculos = () => {
-divResult.innerHTML= ''
-veiculos.forEach((elem, i) => {
+const listaVeiculos = () => { //isso aqui é  função de listar todos o veiculos cadastrados, ela exibe no no divresult.
+divResult.innerHTML= ''  // faz com que o divresult NÃO exiba o reultado anterior junto do resultado novo, "limpando" o resultado para exibir a nova lista.
+veiculos.forEach((elem, i) => { //percorre cada elemento em veículos e faz com que ele seja exibido conforme definido no divresult ali em baixo.
     divResult.innerHTML += `<div class='veiculo-novo'> ${i + 1} - ${elem.marca}, ${elem.modelo}, ${elem.placa}, ${elem.fabricacao},${elem.valor} ${elem.combustivel}`
-})
+}) // o + faz com que "outro divresult" seja exibido, evitando que o anterior seja apagado. 
 }
